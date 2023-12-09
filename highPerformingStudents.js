0aa43683221589086c9202372fd5717e583fcbdf
Subhadip Das <subhadipdas911@gmail.com>
2023-12-09 12:48:49 +0530

    Initialized the problem with the Problem statement

---
+++ highPerformingStudents.js
@@ -0,0 +1,8 @@
+const students = [
+      {name : "Alice", scores:[90,85,92]},
+      {name : "Bob", scores:[75,80,85]},
+      {name : "Charles", scores:[90,95,85]},
+      {name : "David", scores:[100,100,100]}
+];
+
+// Return an array of Objects containing only the students who has an average score of above 90.
