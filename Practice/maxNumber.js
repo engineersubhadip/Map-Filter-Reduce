c3eca6e104bfc5e5acf1e082668d2119587aabaf
Subhadip Das <subhadipdas911@gmail.com>
2023-12-25 15:23:21 +0530

    Maximum Number inan Array

--- Practice/maxNumber.js
+++ Practice/maxNumber.js
@@ -1,1 +1,10 @@
 const numbers = [8, 3, 12, 5, 9];
+
+const result = numbers.reduce(function(acc,curr){
+    if (acc < curr){
+        acc = curr;
+    }
+    return acc;
+},-1e9);
+
+console.log(result);
