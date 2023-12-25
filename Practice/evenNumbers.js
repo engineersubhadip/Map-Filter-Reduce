d6643fe199474dac21db62d57bb20c6a1e5a86ed
Subhadip Das <subhadipdas911@gmail.com>
2023-12-25 15:03:07 +0530

    Filtering out the Even Numbers

--- Practice/evenNumbers.js
+++ Practice/evenNumbers.js
@@ -1,1 +1,11 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const result = numbers.filter(function(element){
+    if (element % 2 == 0){
+        return true;
+    }else{
+        return false;
+    }
+});
+
+console.log(result);
